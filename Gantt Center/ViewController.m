a2dc4fbee0fcbccc8462a315c87a4265c54d350f
AJ Green <ajgreen1987@gmail.com>
2014-11-22 20:00:51 -0500

    Initial Commit

---
+++ Gantt Center/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Gantt Center
+//
+//  Created by AJ Green on 11/22/14.
+//  Copyright (c) 2014 Gantt Center. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
